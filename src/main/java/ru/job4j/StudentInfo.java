e8f21c27acc6879734ca3a3707b15910bdc43a7a
sten2297 <tensergey97@yandex.ru>
2021-11-01 17:40:48 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args) {
+        System.out.println("Sergey Ten");
+        System.out.println("22/09/1997");
+    }
+}
