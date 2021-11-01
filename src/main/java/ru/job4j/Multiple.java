8309ebaaad7f45189ddd014a1fda1c5b851f5656
sten2297 <tensergey97@yandex.ru>
2021-11-01 18:01:18 +0300

    Фиксация изменений 4.1. Разбор первой программы. [#159033 #223268]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 //4.1. Разбор первой программы. [#159033 #223268]
 public class Multiple {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("1 * 1 = 1");
         System.out.println("1 * 2 = 2");
         System.out.println("1 * 3 = 3");
