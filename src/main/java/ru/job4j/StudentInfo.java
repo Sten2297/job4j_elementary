b9a6c392ade086ea1f594121d7308245041b3d4d
sten2297 <tensergey97@yandex.ru>
2021-11-01 18:17:45 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Sergey Ten");
         System.out.println("22/09/1997");
     }
